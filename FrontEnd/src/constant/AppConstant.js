7b371e802091bbed585d56951c45546ae820baee
quyetdv28978 <107168139+quyetdv28978@users.noreply.github.com>
2024-03-12 23:01:19 +0700

    gl

--- FrontEnd/src/constant/AppConstant.js
+++
@@ -1,6 +0,0 @@
-export const APP_CONSTANT = {
-    HINH_THUC_GIAO_HANG: [
-        {id: 1, ten: "Nhận hàng tại quầy"},
-        {id: 2, ten: "Giao hàng"}
-    ]
-}
--- FrontEnd/src/service/axios/axiosPort.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const customPort = 8080; // Đổi thành cổng bạn muốn sử dụng
-
-// Cấu hình Axios sử dụng cổng mặc định
-axios.defaults.baseURL = `http://localhost:${customPort}`;
-
-export default axios;
